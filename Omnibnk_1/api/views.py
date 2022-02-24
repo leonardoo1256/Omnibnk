3c0fb654156a1c2ce41abe4df3b0d5ad1a035b33
Leonardo Ardila <hebert_leonardo06@hotmail.com>
2022-02-24 18:37:37 -0500

    Update

--- Omnibnk_1/api/views.py
+++ Omnibnk_1/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import status, viewsets
 from rest_framework.views import APIView
 from api.models import Movie
 from api.serializers import MovieSerializer
+from django_filters.rest_framework import DjandoFilterBackend
 
 
 def newuser(request):
@@ -53,6 +54,8 @@ def update(request, id):
 class MovieViewSets(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    http_method_names = ['get',]
+    filter_backends = [DjandoFilterBackend,]
 
 
 class Newuser(APIView):
