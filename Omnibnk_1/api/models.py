8a82eaafbb311d976685c7e111461df13aa34bd0
Leonardo Ardila <hebert_leonardo06@hotmail.com>
2022-02-24 20:42:28 -0500

    Update

--- Omnibnk_1/api/models.py
+++ Omnibnk_1/api/models.py
@@ -10,4 +10,12 @@ class Movie(models.Model):
         ordering = ('id',)
         default_permissions = ('add','change','delete','view')
 
+class TvShow(models.Model):
+    name = models.CharField(max_length=100, unique= True, blank=False)
+    genre = models.CharField(max_length=100, blank=False)
+    director = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        ordering = ('id',)
+        default_permissions = ('add','change','delete','view')
 
